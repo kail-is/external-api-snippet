896adc0c6b6907cbc44da8896bbeb9a12628bb9a
jeansuh42 <jeansuh42@gmail.com>
2024-10-10 22:49:53 +0900

    add: test

---
+++ src/test/java/com/kailis/snippet/ExternalApiSnippetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kailis.snippet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalApiSnippetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
